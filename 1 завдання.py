2fa2bbb7a6c6366b3bc65fea5112b437e3e20329
KYUUBI-K <73813970+KYUUBI-K@users.noreply.github.com>
2020-11-10 15:18:29 +0200

    Delete 1 завдання.py

--- 1 завдання.py
+++
@@ -1,7 +0,0 @@
-#знайти площу(S) і периметр ромба(P)
-import math
-side_of_the_rhombus = float (input("Сторони ромба ="))
-angle = float(input("Кут= "))
-S = (side_of_the_rhombus**2)*(math.sin(angle))
-P= side_of_the_rhombus*4
-print('Площа = {0}, Периметр = {1}'.format(S,P))
